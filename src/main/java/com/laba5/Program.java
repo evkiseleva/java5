9a92cec38e517be1e358871f1ed910bd2d9fd38f
Ekaterina <e.kiseleva01@mail.ru>
2022-05-09 16:27:21 +0300

    Добавлены main и file.txt

---
+++ src/main/java/com/laba5/Program.java
@@ -0,0 +1,14 @@
+package com.laba5;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+public class Program {
+
+    public static void main(String[] args) throws IOException, InvocationTargetException, ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException
+    {
+        Injector injector = new Injector("file.txt");
+        SomeBean someBean = injector.inject(new SomeBean());
+        someBean.foo();
+    }
+}
